40674e1f3e4296f2dcc702eeecef92c6b6db7d42
Tyler Matteson <tyler@agritheory.com>
2023-05-16 17:14:25 -0400

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## BEAM
+
+Barcode Scanning for ERPNext
+
+#### License
+
+MIT
---
+++ beam/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "BEAM",
+			"type": "module",
+			"label": _("BEAM")
+		}
+	]
