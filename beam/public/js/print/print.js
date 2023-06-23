2323b825d7e7e94d72be868861bc7d6c6e2b1910
Tyler Matteson <tyler@agritheory.com>
2023-06-23 09:36:45 -0400

    Draft: Print server integration (#14)
    * feat: add print_server integration
    
    * fix: remove bad default  in dialog
    
    * feat: handle scanning of non-handling units
    
    * fix: convert tuple to dict
    
    * ci: fix json diff job
    
    * fix: add stub for Stock Entry-Item barcode scans
    
    * fix: setup stock entry scanning
    
    * fix: set basic rate for item in Stock Entry
    
    * fix: remove reference to client code
    
    * fix: normalize return values for scanning
    
    ---------
    
    Co-authored-by: Rohan Bansal <rohan@parsimony.com>

---
+++ beam/public/js/print/print.js
@@ -0,0 +1,79 @@
+frappe.ui.form.on('Stock Entry', {
+	refresh(frm) {
+		custom_print_button(frm)
+	},
+})
+frappe.ui.form.on('Stock Reconciliation', {
+	refresh(frm) {
+		custom_print_button(frm)
+	},
+})
+frappe.ui.form.on('Purchase Invoice', {
+	refresh(frm) {
+		if (frm.doc.update_stock) {
+			custom_print_button(frm)
+		}
+	},
+})
+frappe.ui.form.on('Purchase Receipt', {
+	refresh(frm) {
+		custom_print_button(frm)
+	},
+})
+frappe.ui.form.on('Sales Invoice', {
+	refresh(frm) {
+		if (frm.doc.update_stock) {
+			custom_print_button(frm)
+		}
+	},
+})
+frappe.ui.form.on('Delivery Note', {
+	refresh(frm) {
+		custom_print_button(frm)
+	},
+})
+
+function custom_print_button(frm) {
+	if (frm.doc.docstatus != 1) {
+		return
+	}
+	frm.add_custom_button(__('<span class="fa fa-print"></span> Print Handling Unit'), () => {
+		let d = new frappe.ui.Dialog({
+			title: __('Select Printer Setting'),
+			fields: [
+				{
+					label: __('Printer Setting'),
+					fieldname: 'printer_setting',
+					fieldtype: 'Link',
+					options: 'Network Printer Settings',
+				},
+				{
+					label: __('Printer Format'),
+					fieldname: 'print_format',
+					fieldtype: 'Link',
+					options: 'Print Format',
+					get_query: function () {
+						return {
+							filters: { doc_type: 'Handling Unit' },
+						}
+					},
+				},
+			],
+			primary_action_label: 'Select',
+			primary_action(selection) {
+				d.hide()
+				frappe.call({
+					method: 'beam.beam.printing.print_handling_units',
+					args: {
+						doctype: frm.doc.doctype,
+						name: frm.doc.name,
+						printer_setting: selection.printer_setting,
+						print_format: selection.print_format,
+						doc: frm.doc,
+					},
+				})
+			},
+		})
+		d.show()
+	})
+}
