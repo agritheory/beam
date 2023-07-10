0e98e9dd4546c2aff58d72058594a69f33728571
Francisco Roldán <franciscoproldan@gmail.com>
2023-07-10 08:31:48 -0300

    Refactor frappe.db.sql calls to query builder (#35)
    * feat: Refactor frappe.db.sql calls to query builder
    
    * feat: added missing return
    
    * Multiple UOMs (#31)
    
    * wip/test: scan tests
    
    * tests: more compelte tests
    
    * docs: handling unit
    
    * wip: refactor to inventory dimension
    
    * wip: refactor to inventory dimensions
    
    * wip/test: scan tests
    
    * test: test manufacture stock entry
    
    * tests: more complete tests
    
    * wip/test: scan tests
    
    * fix: remove merge conflict
    
    * test: add delivery note and sales invoice tests
    
    * feat: integrate warranty calim and putaway rule
    
    * feat: quality inspection
    
    * test: add test for packing slip
    
    * wip: get non-stock uom from source document
    
    * fix: get_handling_unit
    
    * fix: actual_qty renamed as stock_qty in handling_unit()
    
    * fix: actual_qty renamed as stock_qty in handling_unit()
    
    * feat: minor improvement
    
    * fix: trailing comma, pre-commit
    
    * fix: test, delete unused code
    
    * feat: get_handling_unit should calculate stock_qty instead of qty
    
    * fix: pop
    
    ---------
    
    Co-authored-by: Francisco Roldán <franciscoproldan@gmail.com>
    
    * chore: remove purchase invoice item customization
    
    ---------
    
    Co-authored-by: Tyler Matteson <tyler@agritheory.com>

--- beam/beam/doctype/handling_unit/__init__.py
+++ beam/beam/doctype/handling_unit/__init__.py
@@ -1,26 +1,21 @@
-import frappe
-import json
 import datetime
+import json
+
+import frappe
+from pypika import Order
 
 
 @frappe.whitelist()
 @frappe.validate_and_sanitize_search_inputs
 def handling_unit_query(doctype, txt, searchfield, start=0, page_len=20, filters=None):
 	filters = frappe._dict({}) if not filters else filters
-	return frappe.db.sql(
-		"""
-		SELECT
-			`tabStock Ledger Entry`.handling_unit
-		FROM `tabStock Ledger Entry`
-		WHERE `tabStock Ledger Entry`.item_code = %(item_code)s
-		AND `tabStock Ledger Entry`.warehouse = %(warehouse)s
-		ORDER BY `tabStock Ledger Entry`.modified DESC
-		LIMIT %(page_len)s, %(start)s
-	""",
-		{
-			"item_code": filters.get("item_code"),
-			"warehouse": filters.get("warehouse"),
-			"start": start,
-			"page_len": page_len,
-		},
-	)
+	stock_ledger_entry = frappe.qb.DocType("Stock Ledger Entry")
+	return (
+		frappe.qb.from_(stock_ledger_entry)
+		.select(stock_ledger_entry.handling_unit)
+		.where(stock_ledger_entry.item_code == filters.get("item_code"))
+		.where(stock_ledger_entry.warehouse == filters.get("warehouse"))
+		.orderby("modified", order=Order.desc)
+		.limit(page_len)
+		.offset(start)
+	).run()
