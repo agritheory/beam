1d894348f4cfd921f75971ee44563af51e2d3cc8
fproldan <franciscoproldan@gmail.com>
2023-07-21 09:40:55 -0300

    feat: no copy for hu fields, hide hu field based configured doctypes

--- beam/install.py
+++ beam/install.py
@@ -1,5 +1,7 @@
 import frappe
 
+from beam.beam.scan.config import get_scan_doctypes
+
 
 def after_install():
 	print("Setting up Handling Unit Inventory Dimension")
@@ -22,4 +24,19 @@ def after_install():
 		if custom_field.dt == "Purchase Invoice Item":
 			frappe.set_value("Custom Field", custom_field, "label", "Handling Unit")
 		else:
+<<<<<<< HEAD
 			frappe.set_value("Custom Field", custom_field, "read_only", 1)
+=======
+			frappe.set_value("Custom Field", custom_field, "hidden", 1)
+
+	frm_doctypes = get_scan_doctypes()["frm"]
+
+	for custom_field in frappe.get_all("Custom Field", {"label": "Handling Unit"}, ["name", "dt"]):
+		frappe.set_value("Custom Field", custom_field["name"], "no_copy", 1)
+
+		if (
+			custom_field["dt"] not in frm_doctypes
+			and custom_field["dt"].replace(" Item", "").replace(" Detail", "") not in frm_doctypes
+		):
+			frappe.set_value("Custom Field", custom_field["name"], "hidden", 1)
+>>>>>>> ac62bd8 (feat: no copy for hu fields, hide hu field based configured doctypes)
