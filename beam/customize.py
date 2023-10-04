a793210e45a989c87af7085e051a0dfded08b51a
Tyler Matteson <tyler@agritheory.com>
2023-10-04 14:49:24 -0400

    fix: only install beam customizations

--- beam/customize.py
+++ beam/customize.py
@@ -10,6 +10,8 @@ def load_customizations():
 	for file in files:
 		customizations = json.loads(Path(file).read_text())
 		for field in customizations.get("custom_fields"):
+			if field.get("module") != "BEAM":
+				continue
 			existing_field = frappe.get_value("Custom Field", field.get("name"))
 			custom_field = (
 				frappe.get_doc("Custom Field", field.get("name"))
@@ -22,6 +24,8 @@ def load_customizations():
 			custom_field.flags.ignore_version = True
 			custom_field.save()
 		for prop in customizations.get("property_setters"):
+			if field.get("module") != "BEAM":
+				continue
 			property_setter = frappe.get_doc(
 				{
 					"name": prop.get("name"),
