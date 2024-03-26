da908af16099f65920d1b32f2629010231af3d5c
Heather Kusmierz <heather.kusmierz@gmail.com>
2024-03-26 13:11:22 -0400

    ci: refactor env variable, remove unused code (#97)

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -13,8 +13,6 @@ fi
 
 cd ~ || exit
 
-# sudo apt update -y && sudo apt install redis-server -y 
-
 pip install --upgrade pip
 pip install frappe-bench
 
@@ -28,25 +26,8 @@ mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_si
 mysql --host 127.0.0.1 --port 3306 -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'"  # match site_cofig
 mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES"
 
-echo "------ CI VARIABLES ------"
-echo "GITHUB_EVENT_NAME:"
-echo "${GITHUB_EVENT_NAME}"  # push, pull_request
-echo "GITHUB_BASE_REF:"  # only works when event is pull request
-echo "${GITHUB_BASE_REF}"  # blank for push, version-14 for pull_request
-echo "GITHUB_REF_NAME (for push/tag, format for PR is pr#/merge):"
-echo "${GITHUB_REF_NAME}"  # version-15 for push, pr#/merge for pull_request
-echo "--------------------------"
-
-if [ "${GITHUB_EVENT_NAME}" = 'pull_request' ]; then
-    echo "GITHUB_EVENT_NAME IS pull_request"
-    BRANCH_NAME="${GITHUB_BASE_REF}"
-else
-    echo "GITHUB_EVENT_NAME IS push"
-    BRANCH_NAME="${GITHUB_REF_NAME}"
-fi
-echo "BRANCH_NAME IS: ${BRANCH_NAME}"
-
-git clone https://github.com/frappe/frappe --branch "${BRANCH_NAME}"
+echo BRANCH_NAME: "${BRANCH_NAME}"
+git clone https://github.com/frappe/frappe --branch ${BRANCH_NAME}
 bench init frappe-bench --frappe-path ~/frappe --python "$(which python)" --skip-assets --ignore-exist
 
 mkdir ~/frappe-bench/sites/test_site
@@ -59,8 +40,8 @@ sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
-bench get-app https://github.com/frappe/erpnext --branch "${BRANCH_NAME}" --resolve-deps --skip-assets
-bench get-app beam "${GITHUB_WORKSPACE}" --skip-assets --resolve-deps
+bench get-app erpnext https://github.com/frappe/erpnext --branch ${BRANCH_NAME} --resolve-deps --skip-assets
+bench get-app beam "${GITHUB_WORKSPACE}" --skip-assets
 
 printf '%s\n' 'frappe' 'erpnext' 'beam' > ~/frappe-bench/sites/apps.txt
 bench setup requirements --python
@@ -70,7 +51,6 @@ bench start &> bench_run_logs.txt &
 CI=Yes &
 bench --site test_site reinstall --yes --admin-password admin
 
-# bench --site test_site install-app erpnext beam
 bench setup requirements --dev
 
 echo "BENCH VERSION NUMBERS:"
