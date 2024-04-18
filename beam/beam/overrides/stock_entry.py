b565bdc13ff2dc41a2f3af536799ded9c87f0d74
Rohan Bansal <rohan@agritheory.dev>
2024-04-18 11:59:22 +0530

    fix: add read-only decorator to APIs

--- beam/beam/overrides/stock_entry.py
+++ beam/beam/overrides/stock_entry.py
@@ -62,6 +62,7 @@ def set_rows_to_recombine(docname: str, to_recombine=None) -> None:
 
 
 @frappe.whitelist()
+@frappe.read_only()
 def get_handling_units_for_item_code(doctype, txt, searchfield, start, page_len, filters):
 	StockLedgerEntry = frappe.qb.DocType("Stock Ledger Entry")
 	return (
@@ -78,6 +79,7 @@ def get_handling_units_for_item_code(doctype, txt, searchfield, start, page_len,
 
 
 @frappe.whitelist()
+@frappe.read_only()
 def get_handling_unit_qty(voucher_no, handling_unit, warehouse):
 	return frappe.db.get_value(
 		"Stock Ledger Entry",
