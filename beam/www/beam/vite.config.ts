8ecddffaf03fb22b9b2e5038ca8b84e68faddb0a
Rohan Bansal <rohan@agritheory.dev>
2024-05-27 13:12:22 +0530

    feat: add dev script

--- beam/www/beam/vite.config.ts
+++ beam/www/beam/vite.config.ts
@@ -1,67 +1,23 @@
 import vue from '@vitejs/plugin-vue'
-// import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-const projectRootDir = resolve(__dirname)
-
 export default defineConfig({
-	plugins: [
-		vue(),
-		// VitePWA({
-		// 	registerType: "autoUpdate",
-		// 	devOptions: {
-		// 		enabled: true,
-		// 	},
-		// 	manifest: {
-		// 		display: "standalone",
-		// 		name: "Frappe HR",
-		// 		short_name: "Frappe HR",
-		// 		start_url: "/hrms",
-		// 		description: "Everyday HR & Payroll operations at your fingertips",
-		// 		icons: [
-		// 			{
-		// 				src: "/assets/hrms/manifest/manifest-icon-192.maskable.png",
-		// 				sizes: "192x192",
-		// 				type: "image/png",
-		// 				purpose: "any",
-		// 			},
-		// 			{
-		// 				src: "/assets/hrms/manifest/manifest-icon-192.maskable.png",
-		// 				sizes: "192x192",
-		// 				type: "image/png",
-		// 				purpose: "maskable",
-		// 			},
-		// 			{
-		// 				src: "/assets/hrms/manifest/manifest-icon-512.maskable.png",
-		// 				sizes: "512x512",
-		// 				type: "image/png",
-		// 				purpose: "any",
-		// 			},
-		// 			{
-		// 				src: "/assets/hrms/manifest/manifest-icon-512.maskable.png",
-		// 				sizes: "512x512",
-		// 				type: "image/png",
-		// 				purpose: "maskable",
-		// 			},
-		// 		],
-		// 	},
-		// }),
-	],
+	plugins: [vue()],
 	build: {
 		lib: {
-			entry: resolve(projectRootDir, 'beam.js'),
+			entry: resolve(__dirname, 'beam.js'),
 			name: 'beam',
-			fileName: format => `index.js`, // creates module only output
+			formats: ['es'], // creates module only output
+			fileName: format => 'index.js',
 			// TODO: need to figure out how to export index.css also
 		},
-		outDir: './beam/www/beam/',
 		target: 'es2015',
+		outDir: './',
 		emptyOutDir: false,
 		minify: false,
 	},
-	optimizeDeps: {},
 	define: {
 		'process.env': process.env,
-	},
+	}
 })
