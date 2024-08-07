043e331708eb9e4330ab829966a1825c781a48e7
Heather Kusmierz <heather.kusmierz@gmail.com>
2024-08-07 11:14:08 -0400

    docs: update network printer names, screen shot

--- beam/tests/setup.py
+++ beam/tests/setup.py
@@ -575,13 +575,14 @@ def create_purchase_receipt_for_received_qty_test(settings):
 
 def create_network_printer_settings(settings):
 	printer_settings = [
-		{"name": "Primary Example Printer", "server_ip": "localhost", "port": 8888},
-		{"name": "Secondary Example Printer", "server_ip": "localhost", "port": 9999},
+		{"name": "Receiving Printer", "server_ip": "localhost", "port": 8888},
+		{"name": "Kitchen Printer", "server_ip": "localhost", "port": 9999},
 	]
 	for ps in printer_settings:
-		nps = frappe.new_doc("Network Printer Settings")
-		nps.name = ps["name"]
-		nps.server_ip = ps["server_ip"]
-		nps.port = ps["port"]
-		nps.printer_name = ps["name"]
-		nps.save()
+		if not frappe.db.exists("Network Printer Settings", ps):
+			nps = frappe.new_doc("Network Printer Settings")
+			nps.name = ps["name"]
+			nps.server_ip = ps["server_ip"]
+			nps.port = ps["port"]
+			nps.printer_name = ps["name"]
+			nps.save()
