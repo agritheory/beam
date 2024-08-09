e8b07d3b8c13e422f7525a3ecb122ba2d0b93276
Myuddin khatri <khatrimayu111@gmail.com>
2024-08-09 19:34:34 +0530

    feat: reuires login for /beam

--- beam/www/beam/index.ts
+++ beam/www/beam/index.ts
@@ -31,67 +31,93 @@ if (import.meta.env.VITE_SERVER) {
 	makeServer()
 }
 
+declare const frappe: any
+
 const routes: RouteRecordRaw[] = [
 	{
 		path: '/',
 		name: 'home',
 		component: Home,
+		meta: { requiresAuth: true },
 	},
 	{
 		path: '',
 		name: 'home',
 		component: Home,
+		meta: { requiresAuth: true },
 	},
 	{
 		path: '/workstation',
 		name: 'workstation',
 		component: Workstation,
+		meta: { requiresAuth: true },
 	},
 	{
 		path: '/work_order',
 		name: 'work_order',
 		component: WorkOrder,
+		meta: { requiresAuth: true },
 	},
 	{
 		path: '/job_card/:id',
 		name: 'job_card',
 		component: JobCard,
+		meta: { requiresAuth: true },
 	},
 	{
 		path: '/work_order/:workOrder/operation/:id',
 		name: 'operation',
 		component: Operation,
+		meta: { requiresAuth: true },
 	},
 	{
 		path: '/receive',
 		name: 'receive',
 		component: Receive,
+		meta: { requiresAuth: true },
 	},
 	{
 		path: '/ship',
 		name: 'ship',
 		component: Ship,
+		meta: { requiresAuth: true },
 	},
 	{
 		path: '/transfer',
 		name: 'transfer',
 		component: Transfer,
+		meta: { requiresAuth: true },
 	},
 	{
 		path: '/manufacture',
 		name: 'manufacture',
 		component: Manufacture,
+		meta: { requiresAuth: true },
 	},
 	{
 		path: '/repack',
 		name: 'repack',
 		component: Repack,
+		meta: { requiresAuth: true },
 	},
 ]
 
 const router = createRouter({
 	history: createWebHashHistory(),
 	routes,
+})
+
+router.beforeEach((to, from, next) => {
+	if (to.meta && to.meta.requiresAuth) {
+		if (frappe.user == 'Guest') {
+			next(false)
+			window.location.href = '/login'
+		} else {
+			next()
+		}
+	} else {
+		next()
+	}
 })
 
 const app = createApp(Beam)
