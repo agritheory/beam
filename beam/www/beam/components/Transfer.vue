2a077e548ab9ef5a7a0e51bdb4ab6394b774fe47
Lauty <38353324+lauty95@users.noreply.github.com>
2024-09-18 13:42:54 +0000

    fix: using ListView

--- beam/www/beam/components/Transfer.vue
+++ beam/www/beam/components/Transfer.vue
@@ -1,68 +1,27 @@
 <template>
-    <div>
-        <table class="item-table">
-            <thead>
-                <tr>
-                    <th>Components</th>
-                    <th>Quantity</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in items" :key="item.name">
-                    <td>{{ item.item_name }}</td>
-                    <td>{{ item.transferred_qty }}/{{ item.required_qty }}</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    <ListView :items="listItems" />
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
-import type { ListTransferItem } from '../types'
+import { defineProps, ref, watchEffect } from 'vue';
+import type { ListTransferItem, ListViewItem } from '../types'
 
 const props = defineProps<{
     items: ListTransferItem[],
     workOrderId: string | string[],
 }>()
 
+const listItems = ref<ListViewItem[]>([])
+
+watchEffect(() => {
+    listItems.value = props.items?.map(it => ({
+        label: it.item_name,
+        description: `${it.source_warehouse} > ${it.source_warehouse}`,
+        count: {
+            count: 0,
+            of: it.required_qty
+        }
+    }))
+})
+
 </script>
-
-<style scoped>
-.item-table {
-    width: 100%;
-    border-collapse: collapse;
-    background-color: #ffffff;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-.item-table th,
-.item-table td {
-    padding: 12px;
-    text-align: left;
-    border-bottom: 1px solid #ddd;
-}
-
-.item-table th {
-    background-color: #f9f9f9;
-    font-weight: bold;
-}
-
-.item-table tr:nth-child(even) {
-    background-color: #f2f2f2;
-}
-
-.item-table td {
-    font-size: 1rem;
-}
-
-.item-table th:last-child {
-    text-align: right;
-}
-
-.item-table td:last-child {
-    text-align: right;
-    font-weight: bold;
-    color: #007bff;
-}
-</style>
