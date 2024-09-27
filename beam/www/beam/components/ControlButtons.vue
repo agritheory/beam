caacee290fd2dc3a7d5abde52b23a42037ade9a6
Lauty <38353324+lauty95@users.noreply.github.com>
2024-09-27 16:13:50 +0000

    feat: amend and doctype name

--- beam/www/beam/components/ControlButtons.vue
+++ beam/www/beam/components/ControlButtons.vue
@@ -1,15 +1,19 @@
 <template>
 	<div class="control-buttons">
-		<button class="btn" @click="onCreate" :disabled="docstatus !== -1">Save</button>
-		<button class="btn" @click="onSubmit" :disabled="docstatus !== 0">Submit</button>
-		<button class="btn" @click="onCancel" :disabled="docstatus !== 1">Cancel</button>
+		<button class="btn" @click="onAmend" v-if="docstatus === 2">Amend</button>
+		<template v-else>
+			<button class="btn" @click="onSave" :disabled="!(docstatus === 0 && doctypeName === '')">Save</button>
+			<button class="btn" @click="onSubmit" :disabled="!(docstatus === 0 && doctypeName !== '')">Submit</button>
+			<button class="btn" @click="onCancel" :disabled="!(docstatus === 1)">Cancel</button>
+		</template>
 	</div>
 </template>
 
 <script setup lang="ts" generic="T extends ParentDoctype">
 import { ParentDoctype } from '@/types';
 import { defineProps, ref } from 'vue'
-const docstatus = ref(-1)
+const docstatus = ref(0)
+const doctypeName = ref("")
 
 const props = defineProps<{
 	onCreate: () => Promise<{ data: T }>
@@ -17,12 +21,11 @@ const props = defineProps<{
 	onCancel: () => Promise<{ data: T; exception: string; response: Response }>
 }>()
 
-const onCreate = async () => {
+const onSave = async () => {
 	try {
 		const { data } = await props.onCreate()
-		if (data) {
-			docstatus.value = data.docstatus
-		}
+		doctypeName.value = data.name
+		if (data) docstatus.value = data.docstatus
 	} catch (err) {
 		console.error(err)
 	}
@@ -31,9 +34,7 @@ const onCreate = async () => {
 const onSubmit = async () => {
 	try {
 		const { data } = await props.onSubmit()
-		if (data) {
-			docstatus.value = data.docstatus
-		}
+		if (data) docstatus.value = data.docstatus
 	} catch (err) {
 		console.error(err)
 	}
@@ -42,14 +43,16 @@ const onSubmit = async () => {
 const onCancel = async () => {
 	try {
 		const { data } = await props.onCancel()
-		if (data) {
-			docstatus.value = -1
-		}
+		if (data) docstatus.value = 2
 	} catch (err) {
 		console.error(err)
 	}
 }
 
+const onAmend = () => {
+	docstatus.value = 0
+	doctypeName.value = ""
+}
 
 </script>
 
--- beam/www/beam/components/Transfer.vue
+++ beam/www/beam/components/Transfer.vue
@@ -1,5 +1,6 @@
 <template>
 	<ControlButtons
+		:doctypeName="stockEntryId"
 		:onCreate="create"
 		:onSubmit="() => store.submit<StockEntry>('Stock Entry', stockEntryId)"
 		:onCancel="() => store.cancel<StockEntry>('Stock Entry', stockEntryId)"
