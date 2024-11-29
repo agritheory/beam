7b166aaa47c9c074df20385f1e541c9479caf67a
lauty95 <lautaroj95@gmail.com>
2024-11-29 17:56:59 +0000

    feat: implement ADropdown

--- beam/www/beam/pages/Move.vue
+++ beam/www/beam/pages/Move.vue
@@ -9,72 +9,74 @@
 		</template>
 	</Navbar>
 
-	<!-- body section -->
+	<ADropdown
+		label="Source Warehouse"
+		:items="warehouseList"
+		v-model="sourceWarehouse"
+		@filterChanged="filterChanged"
+	/>
+	<ADropdown
+		label="Target Warehouse"
+		:items="warehouseList"
+		v-model="targetWarehouse"
+		@filterChanged="filterChanged"
+	/>
+
 	<ListView :items="listItems" :key="componentKey" />
-	<div class="begin" v-if="listItems.length == 0">
+	<div class="begin" v-if="listItems.length === 0">
 		<span>Scan to Begin</span>
 	</div>
 
-	<!-- footer section -->
-	<ControlButtons
-		:onCreate="create"
-		:onSubmit="() => store.submit<StockEntry>('Stock Entry', stockEntryId)"
-		:onCancel="() => store.cancel<StockEntry>('Stock Entry', stockEntryId)" />
+	<ControlButtons :buttons="controlButtons" />
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { computed, ref, onMounted } from 'vue'
 
 import ControlButtons from '@/components/ControlButtons.vue'
 import { useBeamStore } from '@/stores/beam'
-import type { ListViewItem, StockEntry } from '@/types'
+import type { ControlButton, ListViewItem, StockEntry } from '@/types'
+type Warehouse = {
+	name: string,
+}
 
 const store = useBeamStore()
-
 const listItems = ref<ListViewItem[]>([])
-const stockEntryId = ref('')
 const componentKey = ref(0)
+
+const warehouseList = ref<string[]>([])
+const sourceWarehouse = ref('')
+const targetWarehouse = ref('')
 
 onMounted(async () => {
 	store.form as Partial<StockEntry>
+	await loadWarehouses()
 })
 
-// store.$subscribe((mutation, state) => {
-// 	const parentfield = state.form.doctype === 'Work Order' ? 'required_items' : 'items'
-// 	if (parentfield && state.form[parentfield]) {
-// 		listItems.value = []
-// 		state.form[parentfield].forEach(item => {
-// 			item.wip_warehouse = state.form.wip_warehouse
-// 			listItems.value.push({
-// 				label: item.item_name,
-// 				description: `${item.source_warehouse} > ${item.wip_warehouse}`,
-// 				count: {
-// 					count: item.transferred_qty,
-// 					of: item.required_qty,
-// 				},
-// 			})
-// 		})
-// 		componentKey.value++
-// 	}
-// })
+const controlButtons = computed((): ControlButton[] => {
+	return []
+})
 
-const create = async () => {
-	throw new Error('Not implemented')
+const filterChanged = async (query: string) => {
+	console.log(`onChange: ${query}`)
+}
 
-	// const stockEntry = await store.getMappedStockEntry({
-	// 	work_order_id: sourceId,
-	// 	purpose: 'Material Transfer for Manufacture',
-	// })
+const loadWarehouses = async () => {
+	const warehouses = await store.getAll<Warehouse[]>('Warehouse')
+	warehouseList.value = warehouses.map(warehouse => warehouse.name)
+}
 
-	// const { data, exception, response } = await store.insert('Stock Entry', stockEntry)
-	// if (data.name) {
-	// 	stockEntryId.value = data.name
-	// }
-	// return { data, exception, response }
+const onScan = (scannedValue: string) => {
+	if (!sourceWarehouse.value) {
+		sourceWarehouse.value = scannedValue
+	} else if (!targetWarehouse.value) {
+		targetWarehouse.value = scannedValue
+	}
 }
 </script>
 
-<style scoped>
+<style>
+/* @import url('@stonecrop/aform/styles'); */
 .begin {
 	width: 100%;
 	text-align: center;
