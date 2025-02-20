9cd2df94f307e38d894fb6dd88014f610a5687e8
Lauty <38353324+lauty95@users.noreply.github.com>
2025-02-20 09:41:20 -0300

    Update currentRoute
    Co-authored-by: Rohan <Alchez@users.noreply.github.com>

--- beam/www/beam/stores/scan.ts
+++ beam/www/beam/stores/scan.ts
@@ -19,7 +19,7 @@ export const useScanStore = defineStore('scan', () => {
 
 	const documentId = computed(() => {
 		const currentRoute = store.router.currentRoute.value
-		return currentRoute.params.id || currentRoute.query.id || store.router.currentRoute.value?.name || ''
+		return currentRoute.params.id || currentRoute.query.id || currentRoute.name || ''
 	})
 
 	const mappedDoc = computed(() => store.cache.mappers[documentId.value])
