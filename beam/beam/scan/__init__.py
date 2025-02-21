e7aff00b1e6a88603d103b1e52eed886b7a9b1cb
lauty95 <lautaroj95@gmail.com>
2025-02-21 17:40:07 +0000

    prettier

--- beam/beam/scan/__init__.py
+++ beam/beam/scan/__init__.py
@@ -175,7 +175,7 @@ def get_form_action(barcode_doc: frappe._dict, context: frappe._dict) -> list[di
 		hu_details = get_handling_unit(barcode_doc.doc.name, context.frm)
 		if context.frm == "Stock Entry":
 			if not context.doc:
-				context.doc = { "doctype": "Stock Entry" }
+				context.doc = {"doctype": "Stock Entry"}
 			target = get_stock_entry_item_details(context.doc, hu_details.item_code)
 			target.warehouse = hu_details.warehouse
 		elif context.frm in ("Putaway Rule", "Warranty Claim", "Item Price", "Quality Inspection"):
