cc34baa3b193acaa40eb2389d8fc4800717beafb
lauty95 <lautaroj95@gmail.com>
2025-07-28 12:35:16 +0000

    fix: controls deleted

--- beam/beam/boot.py
+++ beam/beam/boot.py
@@ -16,22 +16,7 @@ def boot_session(bootinfo):
 
 
 def redirect_to_beam():
-	user_agent = frappe.request.headers.get("User-Agent", "").lower()
 	user_roles = frappe.get_roles(frappe.session.user)
 
-	mobile_keywords = [
-		"android",
-		"webos",
-		"iphone",
-		"ipad",
-		"ipod",
-		"blackberry",
-		"iemobile",
-		"opera mini",
-		"mobile",
-	]
-
-	if "BEAM Mobile User" in user_roles or any(agent in user_agent for agent in mobile_keywords):
+	if "BEAM Mobile User" in user_roles:
 		frappe.local.response["home_page"] = "/beam#/"
-		# frappe.local.response["type"] = "redirect"
-		# frappe.local.response["location"] = f"{frappe.utils.get_url()}/beam/"
