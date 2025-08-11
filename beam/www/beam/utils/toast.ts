fd0e04dcafc8d406b304b4fb459e9908a9ed160f
lauty95 <lautaroj95@gmail.com>
2025-08-11 12:52:22 +0000

    fix: copyright

--- beam/www/beam/utils/toast.ts
+++ beam/www/beam/utils/toast.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2024, AgriTheory and contributors
+// For license information, please see license.txt
+
 import { type ToastProps, useToast } from 'vue-toast-notification'
 import 'vue-toast-notification/dist/theme-default.css'
 
