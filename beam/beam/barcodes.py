479b42cc1c14a438be8c164f59de56d78e5183a0
IshwaryaM1030 <ishwaryamohan1030@gmail.com>
2026-01-06 19:15:28 +0530

    Add test for barcode generation in print format (#282)
    Co-authored-by: Ishwarya <ishwarya.m@promantia.com>

--- beam/beam/barcodes.py
+++ beam/beam/barcodes.py
@@ -162,7 +162,12 @@ def add_to_label(label: Label, element: Printable):
 
 class ZPLLabelStringOutput(Label):
 	def __init__(
-		self, width: int = 100, length: int = 100, dpi: int = 203, print_speed: int = 2, copies: int = 1
+		self,
+		width: int = 100,
+		length: int = 100,
+		dpi: int = 203,
+		print_speed: int = 2,
+		copies: int = 1,
 	):
 		super().__init__(width, length, dpi, print_speed, copies)
 
