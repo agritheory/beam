f62f59075c62b70f3a111b9844167ffddcaf2e25
Ishwarya <ishwaryamohan1030@gmail.com>
2026-01-07 13:42:34 +0530

    Pushing the pyproject.toml file

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
-# Copyright (c) 2026, AgriTheory and contributors
+# Copyright (c) 2025, AgriTheory and contributors
 # For license information, please see license.txt
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-	name="beam",
-	version="14.8.7",
-	packages=find_packages(),
-	include_package_data=True,
-	zip_safe=False,
-)
+# TODO: Remove this file when bench >=v5.11.0 is adopted / v15.0.0 is released
+name = "beam"
+
+setup()
