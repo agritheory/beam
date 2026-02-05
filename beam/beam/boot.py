0c324d9c5cc56d446b05fef0d62a570e17fdfdb7
Tyler Matteson <tyler@agritheory.com>
2026-02-05 18:56:37 -0500

    fix: load default print format from get_meta

--- beam/beam/boot.py
+++ beam/beam/boot.py
@@ -9,7 +9,9 @@ from beam.beam.scan.config import get_scan_doctypes
 def boot_session(bootinfo):
 	bootinfo.beam = get_scan_doctypes()
 	bootinfo.beam["settings"] = get_beam_settings()
-	bootinfo.beam["default_hu_print_format"] = get_handling_unit_default_print_format()
+	bootinfo.beam["default_hu_print_format"] = frappe.get_meta("Handling Unit").get(
+		"default_print_format"
+	)
 
 
 def get_beam_settings():
@@ -24,8 +26,3 @@ def get_beam_settings():
 			"enable_handling_units": setting.enable_handling_units,
 		}
 	return settings
-
-
-def get_handling_unit_default_print_format():
-	"""Get the default print format for Handling Unit doctype."""
-	return frappe.db.get_value("DocType", "Handling Unit", "default_print_format")
