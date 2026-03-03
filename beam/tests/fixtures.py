519fa86ea1fd1d2e0c41612ba5820a65850f371a
IshwaryaM1030 <ishwaryamohan1030@gmail.com>
2026-03-03 19:22:11 +0530

    Mobile Repack Tests (#333)
    Co-authored-by: Tyler Matteson <support@agritheory.dev>

--- beam/tests/fixtures.py
+++ beam/tests/fixtures.py
@@ -728,7 +728,7 @@ employees = [
 		},
 		"phone": "(704) 885-0542",
 		"roles": ["Stock Manager", "Item Manager"],
-		"department": "Management - APC",
+		# "department": "Operations",
 		"designation": "Bakery Manager",
 	},
 	{
@@ -745,7 +745,7 @@ employees = [
 		"phone": "(658) 583-5499",
 		"roles": ["Stock User", "BEAM Mobile User"],
 		"reports_to": "Tristan Hawkins",
-		"department": "Operations - APC",
+		# "department": "Operations",
 		"designation": "Baker",
 	},
 	{
@@ -762,7 +762,7 @@ employees = [
 		"phone": "(962) 762-5895",
 		"roles": ["Stock User", "BEAM Mobile User"],
 		"reports_to": "Tristan Hawkins",
-		"department": "Operations - APC",
+		# "department": "Operations",
 		"designation": "Baker",
 	},
 	{
@@ -779,7 +779,7 @@ employees = [
 		"phone": "(366) 357-8223",
 		"roles": ["Stock User", "BEAM Mobile User"],
 		"reports_to": "Tristan Hawkins",
-		"department": "Operations - APC",
+		# "department": "Operations",
 		"designation": "Bakery Manager",
 	},
 	{
@@ -796,7 +796,7 @@ employees = [
 		"phone": "(930) 920-4520",
 		"roles": ["Stock User", "BEAM Mobile User"],
 		"reports_to": "Tristan Hawkins",
-		"department": "Operations - APC",
+		# "department": "Operations",
 		"designation": "Baker",
 	},
 	{
@@ -813,7 +813,7 @@ employees = [
 		"phone": "(054) 893-8970",
 		"roles": ["Stock User", "BEAM Mobile User"],
 		"reports_to": "Tristan Hawkins",
-		"department": "Operations - APC",
+		# "department": "Operations",
 		"designation": "Baker",
 	},
 	{
@@ -830,7 +830,7 @@ employees = [
 		"phone": "(814) 677-9322",
 		"roles": ["Stock User", "BEAM Mobile User"],
 		"reports_to": "Tristan Hawkins",
-		"department": "Operations - APC",
+		# "department": "Operations",
 		"designation": "Baker",
 	},
 	{
@@ -847,7 +847,7 @@ employees = [
 		"phone": "(133) 195-7828",
 		"roles": ["Stock User", "BEAM Mobile User"],
 		"reports_to": "Tristan Hawkins",
-		"department": "Operations - APC",
+		# "department": "Operations",
 		"designation": "Baker",
 	},
 	{
@@ -864,7 +864,7 @@ employees = [
 		"phone": "(041) 000-2569",
 		"roles": ["Stock User", "BEAM Mobile User"],
 		"reports_to": "Tristan Hawkins",
-		"department": "Operations - APC",
+		# "department": "Operations",
 		"designation": "Baker",
 	},
 ]
