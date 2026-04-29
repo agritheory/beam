89662c91acd66e8c85237c446307710861176999
lauty95 <lautaroj95@gmail.com>
2026-04-29 17:00:39 +0000

    feat: job card and job card time log types

--- beam/www/beam/types/frappe.ts
+++ beam/www/beam/types/frappe.ts
@@ -56,6 +56,14 @@ export type User = ParentDoctype & {
 	full_name?: string
 }
 
+export type JobCardTimeLog = ChildDoctypeMeta & {
+	from_time?: string
+	to_time?: string
+	time_in_mins?: number
+	completed_qty?: number
+	employee?: string
+}
+
 export type JobCard = ParentDoctype & {
 	current_time?: number
 	total_time_in_mins: number
@@ -63,7 +71,9 @@ export type JobCard = ParentDoctype & {
 	started_time?: string
 	status?: 'Open' | 'Work In Progress' | 'Material Transferred' | 'On Hold' | 'Submitted' | 'Cancelled' | 'Complete' | 'Completed'
 	total_completed_qty?: number
+	active_job_card_for_employee?: string | null
 	items?: JobCardItem[]
+	time_logs?: JobCardTimeLog[]
 }
 
 export type JobCardItem = ChildDoctype & {
